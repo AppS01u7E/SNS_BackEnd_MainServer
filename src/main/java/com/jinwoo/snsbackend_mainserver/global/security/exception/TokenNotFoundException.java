a19202cb2ffc293e6802c4dcb8d76617d8d5ab17
jinu <jinwoo794533@gmail.com>
2021-11-06 21:17:38 +0900

    TokenNotFOund Exception

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/security/exception/TokenNotFoundException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/security/exception/TokenNotFoundException.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class TokenNotFound extends GlobalException {
-    public TokenNotFound() {
+public class TokenNotFoundException extends GlobalException {
+    public TokenNotFoundException() {
         super(ErrorCode.TOKEN_NOT_FOUND);
     }
 }
