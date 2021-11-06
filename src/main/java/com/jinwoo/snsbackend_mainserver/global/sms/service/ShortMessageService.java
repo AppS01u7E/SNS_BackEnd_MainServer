6ca49e6823509ac94edc4137b02d4af88528a1eb
jinu <jinwoo794533@gmail.com>
2021-11-06 22:12:04 +0900

    Sms

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/sms/service/ShortMessageService.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/sms/service/ShortMessageService.java
@@ -1,2 +1,7 @@
-package com.jinwoo.snsbackend_mainserver.global.sms.service;public interface ShortMessageService {
+package com.jinwoo.snsbackend_mainserver.global.sms.service;
+
+public interface ShortMessageService {
+
+    String sendSMS(String toNumber, String randomNumber);
+
 }
