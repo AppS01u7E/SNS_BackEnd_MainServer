f50b29b34084d9a6757bc9ee31718d2c12d0215e
jinu <jinwoo794533@gmail.com>
2021-11-06 21:16:23 +0900

    AuthService

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/service/AuthServiceImpl.java
@@ -1,2 +1,15 @@
-package com.jinwoo.snsbackend_mainserver.domain.auth.service;public class AuthServiceImpl {
+package com.jinwoo.snsbackend_mainserver.domain.auth.service;
+
+import com.jinwoo.snsbackend_mainserver.global.security.payload.TokenResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+
+    @Override
+    public TokenResponse Signup() {
+
+
+        return null;
+    }
 }
