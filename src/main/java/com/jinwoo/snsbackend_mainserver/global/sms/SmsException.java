c7109539ebd815a31e2d0441bea9e8764aac064a
jinu <jinwoo794533@gmail.com>
2021-11-06 22:12:12 +0900

    SMS exception

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/sms/SmsException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/sms/SmsException.java
@@ -1,2 +1,13 @@
-package com.jinwoo.snsbackend_mainserver.global.sms.service;public class CoolsmsException {
+package com.jinwoo.snsbackend_mainserver.global.sms;
+
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class SmsException extends RuntimeException {
+    public SmsException(CoolsmsException e) {
+        super(e.getMessage());
+    }
+
 }
