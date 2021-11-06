30ef4dd2b3096b3bbd439d549ae10c74d73a3b7f
jinu <jinwoo794533@gmail.com>
2021-11-06 21:01:49 +0900

    MemberDao set Jpa

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
@@ -1,2 +1,10 @@
-package com.jinwoo.snsbackend_mainserver.domain.auth.dao;public interface MemberRepository {
+package com.jinwoo.snsbackend_mainserver.domain.auth.dao;
+
+import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+
+
 }
