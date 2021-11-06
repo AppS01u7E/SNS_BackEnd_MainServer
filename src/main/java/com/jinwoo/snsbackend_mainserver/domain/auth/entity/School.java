780f281692495452c22707398779a14770c87c01
jinu <jinwoo794533@gmail.com>
2021-11-06 21:02:30 +0900

    Embeddable School 정의

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/entity/School.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/entity/School.java
@@ -5,4 +5,10 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class School {
+    //학교정보 및 학반번호까지 모두 포함
+    private String name;
+    private int id;
+    private int grade;
+    private int academicInfo;
+    //1학년 3반 == 103
 }
