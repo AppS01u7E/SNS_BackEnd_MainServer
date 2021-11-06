891507e97d67b0fd33a11483a969733a8bfb77c2
jinu <jinwoo794533@gmail.com>
2021-11-06 21:02:38 +0900

    InvalidTokenException

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/InvalidTokenException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/InvalidTokenException.java
@@ -1,2 +1,14 @@
-package com.jinwoo.snsbackend_mainserver.domain.auth.exception;public class InvalidTokenException {
+package com.jinwoo.snsbackend_mainserver.domain.auth.exception;
+
+import com.jinwoo.snsbackend_mainserver.global.exception.ErrorCode;
+import com.jinwoo.snsbackend_mainserver.global.exception.GlobalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class InvalidTokenException extends GlobalException {
+    public InvalidTokenException() {
+        super(ErrorCode.INVALID_TOKEN);
+    }
 }
