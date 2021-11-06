4cc42253c547cff0c350b287d9d63f811b8dff38
jinu <jinwoo794533@gmail.com>
2021-11-06 21:01:31 +0900

    Auth signup framework

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
@@ -1,2 +1,25 @@
-package com.jinwoo.snsbackend_mainserver.domain.auth.controller;public class AuthController {
+package com.jinwoo.snsbackend_mainserver.domain.auth.controller;
+
+
+import com.jinwoo.snsbackend_mainserver.domain.auth.payload.request.SignupRequest;
+import com.jinwoo.snsbackend_mainserver.global.security.payload.TokenResponse;
+import com.jinwoo.snsbackend_mainserver.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public TokenResponse signup(@RequestBody SignupRequest signupDto){
+
+    }
+
+
 }
