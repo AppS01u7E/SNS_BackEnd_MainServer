cbce7271748beab43c3c8237118bf4edc3101583
jinu <jinwoo794533@gmail.com>
2021-11-06 21:17:55 +0900

    GlobalException

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/exception/GlobalException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/exception/GlobalException.java
@@ -1,2 +1,15 @@
-package com.jinwoo.snsbackend_mainserver.global.exception;public class GlobalException {
+package com.jinwoo.snsbackend_mainserver.global.exception;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class GlobalException extends RuntimeException{
+    private ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }
