472bc74007c32b81c6c2b7c2ad799bb742d41b50
jinu <jinwoo794533@gmail.com>
2021-11-06 22:11:20 +0900

    Cors 설정

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/config/MvcConfig.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/config/MvcConfig.java
@@ -1,2 +1,22 @@
-package com.jinwoo.snsbackend_mainserver.global.config;public class MvcConfig {
+package com.jinwoo.snsbackend_mainserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry
+                .addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","POST","PATCH","DELETE","PUT","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+
+    }
 }
