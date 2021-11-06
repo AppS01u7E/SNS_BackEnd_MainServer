681e82ce1289e806b34b17a0950b82753f9b495b
jinu <jinwoo794533@gmail.com>
2021-11-06 22:11:38 +0900

    잡것

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/security/config/SecurityConfig.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -17,6 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf().disable()
                 .cors().disable()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
 
                 .authorizeRequests()
                     .antMatchers("/api/auth").permitAll()
