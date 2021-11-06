2cfac6c7906c156b69797b6fae94ed656dca4f38
jinu <jinwoo794533@gmail.com>
2021-11-06 22:09:50 +0900

    Service 연결

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.jinwoo.snsbackend_mainserver.domain.auth.controller;
 
 
+import com.jinwoo.snsbackend_mainserver.domain.auth.payload.request.LoginRequest;
 import com.jinwoo.snsbackend_mainserver.domain.auth.payload.request.SignupRequest;
 import com.jinwoo.snsbackend_mainserver.global.security.payload.TokenResponse;
 import com.jinwoo.snsbackend_mainserver.domain.auth.service.AuthService;
@@ -16,10 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/signup")
-    public TokenResponse signup(@RequestBody SignupRequest signupDto){
 
+    @PostMapping("/phone")
+    public String phoneCert(@RequestBody String phoneNum){
+        return "문자 전송 활성화시 발생되는 요금으로 인해 개발 과정 중 메시지 전송은 작동하지 않습니다. \n" +
+                "언제든 SMS를 활성화 할 순 있으나, 개발 기간 중 활성화는 특별한 사안이 아닐시 작동하지 않습니다.\n" +
+                "프론트 개발시 이 url에 요청시 HttpStatusCode 200 이 뜨는지만 확인한 후, 회원가입은 포스트맨으로 진행해주세요.\n" +
+                "인증번호: " + authService.sendSms(phoneNum);
     }
+
+    @PostMapping("/signup")
+    public TokenResponse signup(@RequestBody SignupRequest signupRequest){
+        return authService.signup(signupRequest);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody LoginRequest loginRequest){
+        return authService.login(loginRequest);
+    }
+
 
 
 }
