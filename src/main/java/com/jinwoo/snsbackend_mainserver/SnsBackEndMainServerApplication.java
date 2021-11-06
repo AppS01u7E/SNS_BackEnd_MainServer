8c5ee21708b5e171d2f65f02fe19b4a6dcd3359b
jinu <jinwoo794533@gmail.com>
2021-11-06 16:14:47 +0900

    INIT

---
+++ src/main/java/com/jinwoo/snsbackend_mainserver/SnsBackEndMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.jinwoo.snsbackend_mainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsBackEndMainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsBackEndMainServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinwoo/snsbackend_mainserver/SnsBackEndMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinwoo.snsbackend_mainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsBackEndMainServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
