815ce23018a675542178dff7f9fb1ff91eafca6e
jinu <jinwoo794533@gmail.com>
2021-11-06 21:18:14 +0900

    SecurityConfig add Filter and Settings

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/security/config/SecurityConfig.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/security/config/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.jinwoo.snsbackend_mainserver.global.security.config;public class SecurityConfig {
+package com.jinwoo.snsbackend_mainserver.global.security.config;
+
+import com.jinwoo.snsbackend_mainserver.global.security.component.JwtFilter;
+import com.jinwoo.snsbackend_mainserver.global.security.service.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .cors().disable()
+
+                .authorizeRequests()
+                    .antMatchers("/api/auth").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
 }
