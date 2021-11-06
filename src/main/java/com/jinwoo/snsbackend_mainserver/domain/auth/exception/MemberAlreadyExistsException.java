3e4f310f59c6c43f802fab8ac51a94a63d7769eb
jinu <jinwoo794533@gmail.com>
2021-11-06 22:10:50 +0900

    MemberAlreadyExistsException

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/MemberAlreadyExistsException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/MemberAlreadyExistsException.java
@@ -1,2 +1,14 @@
-package com.jinwoo.snsbackend_mainserver.domain.auth.exception;public class MemberAlreadyExistsException {
+package com.jinwoo.snsbackend_mainserver.domain.auth.exception;
+
+import com.jinwoo.snsbackend_mainserver.global.exception.ErrorCode;
+import com.jinwoo.snsbackend_mainserver.global.exception.GlobalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class MemberAlreadyExistsException extends GlobalException {
+    public MemberAlreadyExistsException() {
+        super(ErrorCode.MEMBER_ALREADY_EXISTS_WITH_ID);
+    }
 }
