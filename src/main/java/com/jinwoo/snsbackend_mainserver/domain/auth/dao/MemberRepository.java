0b303770d091311c2bc4ec9003244f243c948bca
jinu <jinwoo794533@gmail.com>
2021-11-11 21:29:12 +0900

    :card_file_box: Redis 추가 및 MemberRepo

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
@@ -2,9 +2,8 @@ package com.jinwoo.snsbackend_mainserver.domain.auth.dao;
 
 import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
-
-
-
 }
