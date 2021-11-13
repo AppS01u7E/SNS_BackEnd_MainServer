7927cb5e49ea2d9eb18f3c371d122d32315b8a52
jinu <jinwoo794533@gmail.com>
2021-11-13 19:10:38 +0900

    :sparkles: getCurrentMember

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/utils/CurrentMember.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/utils/CurrentMember.java
@@ -1,2 +1,25 @@
-package com.jinwoo.snsbackend_mainserver.global.utils;public class CurrentMember {
+package com.jinwoo.snsbackend_mainserver.global.utils;
+
+
+import com.jinwoo.snsbackend_mainserver.domain.auth.dao.MemberRepository;
+import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
+import com.jinwoo.snsbackend_mainserver.domain.auth.exception.MemberNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentMember {
+    private final MemberRepository memberRepository;
+
+    public String getMemberPk(){
+        return (String) SecurityContextHolder.getContext().getAuthentication().getCredentials();
+    }
+
+    public Member getMember(){
+        return memberRepository.findById(getMemberPk()).orElseThrow(MemberNotFoundException::new);
+    }
+
+
 }
