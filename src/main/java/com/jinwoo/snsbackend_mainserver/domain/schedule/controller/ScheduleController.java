fc4d749d496583442435e7aa7eb74f8a429d8ae4
jinu <jinwoo794533@gmail.com>
2021-11-13 19:04:34 +0900

    :sparkles: schedule 기능 대부분 완성

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/schedule/controller/ScheduleController.java
@@ -1,14 +1,13 @@
 package com.jinwoo.snsbackend_mainserver.domain.schedule.controller;
 
-import com.jinwoo.snsbackend_mainserver.domain.auth.exception.MemberAlreadyExistsException;
-import com.jinwoo.snsbackend_mainserver.domain.schedule.payload.LocalScheReturnResponseDayDto;
+import com.jinwoo.snsbackend_mainserver.domain.schedule.entity.ScheBlockInfo;
+import com.jinwoo.snsbackend_mainserver.domain.schedule.payload.request.IdentifyScheInfoBlockRequest;
+import com.jinwoo.snsbackend_mainserver.domain.schedule.payload.request.WriteScheBlockInfoRequest;
+import com.jinwoo.snsbackend_mainserver.domain.schedule.payload.response.LocalScheReturnResponseDayDto;
 import com.jinwoo.snsbackend_mainserver.domain.schedule.service.ScheduleService;
-import com.jinwoo.snsbackend_mainserver.domain.schedule.service.ScheduleServiceImpl;
+import com.jinwoo.snsbackend_mainserver.global.utils.RedisUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import neiseApi.School;
-import neiseApi.payload.schoolInfo.SchoolShorten;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -22,11 +21,40 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
 
-    @GetMapping("/{grade}/{classNum}")
-    public List<LocalScheReturnResponseDayDto> getSchedule(@RequestParam String schoolCode, @RequestParam int startDate, @RequestParam int endDate,
-                                                           @PathVariable int grade, @PathVariable int classNum) throws IOException {
 
-        return scheduleService.getSchedule(grade, classNum, startDate, endDate, schoolCode);
+
+    @GetMapping("/{grade}/{classNum}")
+    public ScheBlockInfo getRangeSchedule(@PathVariable int grade, @PathVariable int classNum, @RequestParam String schoolCode,
+                                          @RequestParam int period, @RequestParam int sepDate) throws IOException {
+        return scheduleService.getSepSchduleInfo(grade, classNum, schoolCode, period, sepDate);
     }
+
+
+    @GetMapping("/{grade}/{classNum}")
+    public List<LocalScheReturnResponseDayDto> getRangeSchedule(@RequestParam String schoolCode, @RequestParam int startDate, @RequestParam int endDate,
+                                                           @PathVariable int grade, @PathVariable int classNum) throws IOException {
+        return (List<LocalScheReturnResponseDayDto>) scheduleService.getRangeSchedule(grade, classNum, startDate, endDate, schoolCode);
+    }
+
+
+    @PostMapping
+    public WriteScheBlockInfoRequest writeInfoScheduleBlock(@RequestBody WriteScheBlockInfoRequest request){
+        return scheduleService.writeInfo(request);
+    }
+
+    @PatchMapping
+    public WriteScheBlockInfoRequest editInfoScheduleBlock(@RequestBody WriteScheBlockInfoRequest request){
+        return scheduleService.editInfo(request);
+    }
+
+    @DeleteMapping
+    public String deleteScheduleBlock(@RequestBody IdentifyScheInfoBlockRequest request){
+        scheduleService.deleteInfo(request);
+        return "Success";
+    }
+
+
+
+
 
 }
