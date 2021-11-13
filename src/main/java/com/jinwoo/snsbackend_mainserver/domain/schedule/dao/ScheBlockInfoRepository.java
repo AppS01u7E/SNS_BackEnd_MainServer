aab783ca3585f4633ab5a44356a0c602ca2dea5d
jinu <jinwoo794533@gmail.com>
2021-11-13 19:05:02 +0900

    :sparkles: ScheBlock DAO

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/schedule/dao/ScheBlockInfoRepository.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/schedule/dao/ScheBlockInfoRepository.java
@@ -1,2 +1,18 @@
-package com.jinwoo.snsbackend_mainserver.domain.schedule.dao;public interface ScheBlockRepository {
+package com.jinwoo.snsbackend_mainserver.domain.schedule.dao;
+
+import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
+import com.jinwoo.snsbackend_mainserver.domain.schedule.entity.ScheBlockInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface ScheBlockInfoRepository extends JpaRepository<ScheBlockInfo, Long> {
+    Optional<ScheBlockInfo> findByGradeAndClassNumAndDateAndPeriod(int grade, int classNum, LocalDate date,
+                                                                   int period);
+    Optional<ScheBlockInfo> findByGradeAndClassNumAndDateAndPeriodAndWriter(int grade, int classNum, LocalDate date,
+                                                                            int period, String writer);
+
 }
