54f401fef50ab63ba318946ee7c462502dcc263a
jinu <jinwoo794533@gmail.com>
2021-11-13 19:02:14 +0900

    :zap: 사소한 기능

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/entity/School.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/entity/School.java
@@ -1,17 +1,23 @@
 package com.jinwoo.snsbackend_mainserver.domain.auth.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class School {
     //학교정보 및 학반번호까지 모두 포함
     private String schoolName;
-    private int scoolCode;
+    private String areaCode;
+    private String scoolCode;
     private int grade;
     private int classNum;
-    //1학년 3반 == 103
 }
--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/MemberNotFoundException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/exception/MemberNotFoundException.java
@@ -1,7 +1,7 @@
 package com.jinwoo.snsbackend_mainserver.domain.auth.exception;
 
-import com.jinwoo.snsbackend_mainserver.global.exception.ErrorCode;
-import com.jinwoo.snsbackend_mainserver.global.exception.GlobalException;
+import com.jinwoo.snsbackend_mainserver.global.exception.base.ErrorCode;
+import com.jinwoo.snsbackend_mainserver.global.exception.base.GlobalException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
