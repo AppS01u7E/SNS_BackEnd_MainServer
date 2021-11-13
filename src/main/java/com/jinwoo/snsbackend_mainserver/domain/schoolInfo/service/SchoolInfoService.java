9e63be0fedc1b18b0ac568ec316c8ef9dd9f5b9c
jinu <jinwoo794533@gmail.com>
2021-11-13 19:06:33 +0900

    :sparkles: schoolInfoService Add

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/schoolInfo/service/SchoolInfoService.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/schoolInfo/service/SchoolInfoService.java
@@ -1,2 +1,12 @@
-package com.jinwoo.snsbackend_mainserver.domain.schoolInfo.service;public interface schoolInfoService {
+package com.jinwoo.snsbackend_mainserver.domain.schoolInfo.service;
+
+import com.jinwoo.snsbackend_mainserver.domain.schoolInfo.payload.LocalSchoolShortenResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface SchoolInfoService {
+
+
+    public List<LocalSchoolShortenResponse> getSchoolInfo(String schoolName) throws IOException;
 }
--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/schoolInfo/service/SchoolInfoServiceImpl.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/schoolInfo/service/SchoolInfoServiceImpl.java
@@ -1,2 +1,34 @@
-package com.jinwoo.snsbackend_mainserver.domain.schoolInfo.service;public class SchoolInfoServiceImpl {
+package com.jinwoo.snsbackend_mainserver.domain.schoolInfo.service;
+
+
+import com.jinwoo.snsbackend_mainserver.domain.schoolInfo.payload.LocalSchoolShortenResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import neiseApi.School;
+import neiseApi.payload.schoolInfo.SchoolShorten;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class SchoolInfoServiceImpl implements SchoolInfoService{
+
+    @Value("${neis.serviceKey}")
+    private String serviceKey;
+
+    public List<LocalSchoolShortenResponse> getSchoolInfo(String schoolName) throws IOException {
+        neiseApi.School school = new School(serviceKey);
+        return school.getSchoolDetailInfo(schoolName).stream().map(
+                schoolShorten -> new LocalSchoolShortenResponse(schoolShorten.getCode(), schoolShorten.getName(), schoolShorten.getAreaCode()
+                , schoolShorten.getAddressCode(), schoolShorten.getHomePage(), schoolShorten.getTelephone(),
+                        schoolShorten.getType(), schoolShorten.getKind().toString())
+        ).collect(Collectors.toList());
+    }
+
+
 }
