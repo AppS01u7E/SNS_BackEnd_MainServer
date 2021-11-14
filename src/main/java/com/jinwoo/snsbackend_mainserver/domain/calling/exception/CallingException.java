a31cdd942e69f0925b708f07af2e1ba76d4dc8b2
jinu <jinwoo794533@gmail.com>
2021-11-15 08:50:08 +0900

    :sparkles: exception Add

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/calling/exception/CallingException.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/calling/exception/CallingException.java
@@ -1,2 +1,13 @@
-package com.jinwoo.snsbackend_mainserver.domain.calling.exception;public class CallingException {
+package com.jinwoo.snsbackend_mainserver.domain.calling.exception;
+
+import com.jinwoo.snsbackend_mainserver.global.exception.base.ErrorCode;
+import com.jinwoo.snsbackend_mainserver.global.exception.base.GlobalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CallingException extends RuntimeException {
+    public CallingException(String message){
+        super(message);
+    }
 }
--- src/main/java/com/jinwoo/snsbackend_mainserver/global/exception/base/ErrorCode.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/exception/base/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
     MEMBER_ALREADY_EXISTS_WITH_ID("해당 Id의 사용자가 이미 존재합니다.", HttpStatus.CONFLICT),
     INCORRECT_PASSWORD("유효하지 않은 패스워드 입니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND_SCHEDULE_INFO("해당 시간의 메모가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    DATA_CANNOT_BRING("데이터를 가져올 수 업습니다.", HttpStatus.BAD_GATEWAY);
+    DATA_CANNOT_BRING("데이터를 가져올 수 업습니다.", HttpStatus.BAD_GATEWAY),
+    LOWER_AUTHENTICATION("작업을 수행하기에는 너무 낮은 권한입니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus httpStatus;
