4d1cd80bf7569b02be8485bb4a9bfcc2e55375d9
jinu <jinwoo794533@gmail.com>
2021-11-15 08:48:14 +0900

    :sparkles: Fcm settings

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/firebase/service/FcmService.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/firebase/service/FcmService.java
@@ -1,2 +1,9 @@
-package com.jinwoo.snsbackend_mainserver.global.firebase.service;public interface FcmService {
+package com.jinwoo.snsbackend_mainserver.global.firebase.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+
+public interface FcmService {
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException;
 }
