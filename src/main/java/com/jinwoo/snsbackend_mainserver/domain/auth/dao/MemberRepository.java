bcf74245b38e60cb45cdd501462647eecccac15b
jinu <jinwoo794533@gmail.com>
2021-11-15 08:46:14 +0900

    :zap: fiindBy Add

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
@@ -1,9 +1,16 @@
 package com.jinwoo.snsbackend_mainserver.domain.auth.dao;
 
 import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
+import com.jinwoo.snsbackend_mainserver.domain.auth.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
+    //callingService
+    List<Member> findAllBySchool(School school);
+    Optional<Member> findBySchoolAndId(School school, String id);
 }
