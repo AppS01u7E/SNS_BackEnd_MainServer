d56874e4bfbdbcd4ab5d03c8774ee55048f29ef9
jinu <jinwoo794533@gmail.com>
2021-11-20 23:03:53 +0900

    :sparkles: 추가

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/soom/entity/Notice.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/soom/entity/Notice.java
@@ -1,2 +1,71 @@
-package com.jinwoo.snsbackend_mainserver.domain.soom.entity;public class notice {
+package com.jinwoo.snsbackend_mainserver.domain.soom.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.jinwoo.snsbackend_mainserver.global.utils.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+import javax.persistence.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Getter
+public class Notice{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String info;
+
+    @ElementCollection
+    private List<String> fileKeys = new ArrayList<>();
+
+    @OneToMany
+    @JsonBackReference
+    @JoinColumn(name = "comment_id")
+    private List<Comment> comments;
+
+
+    @ManyToOne
+    @JsonBackReference
+    private SoomRoom room;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+
+
+    public Notice(Long id, String title, String info, List<String> fileKeys) {
+        this.id = id;
+        this.title = title;
+        this.info = info;
+        this.fileKeys = fileKeys;
+    }
+
+    public Notice addFiles(List<String> keys){
+        this.fileKeys.addAll(keys);
+        return this;
+    }
+
+    public Notice deleteFile(String key){
+        this.fileKeys.remove(key);
+        return this;
+    }
+
+
 }
--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/soom/entity/SoomRoom.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/soom/entity/SoomRoom.java
@@ -1,13 +1,16 @@
 package com.jinwoo.snsbackend_mainserver.domain.soom.entity;
 
 
-import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.jinwoo.snsbackend_mainserver.domain.soom.dto.request.PostNoticeRequest;
+import com.jinwoo.snsbackend_mainserver.global.utils.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,22 +19,70 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Club {
+public class SoomRoom extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    private String ownerId;
+    private String representativeId;
 
     @ElementCollection
     private List<String> memberIds = new ArrayList<>();
 
-    private String info;
+    private String title;
 
+    private String info;
 
     private String teacherId;
 
+    @ElementCollection
+    private List<URL> profiles = new ArrayList<>();
 
+    @OneToMany
+    @JsonBackReference
+    private List<Notice> notices = new ArrayList<>();
+
+    private SoomType soomType;
+
+
+    private String joinCode;
+
+    public SoomRoom setJoinCode(String code){
+        this.joinCode = code;
+        return this;
+    }
+
+
+    public void addProfile(URL p){
+        this.profiles.add(p);
+    }
+
+    public SoomRoom writeNoticeAndFile(PostNoticeRequest request, List<String> files){
+        this.notices.add(new Notice().builder()
+                        .title(request.getTitle())
+                        .info(request.getInfo())
+                        .fileKeys(files)
+                        .build()
+                );
+        return this;
+    }
+
+    public SoomRoom writeNotice(PostNoticeRequest request){
+        this.notices.add(new Notice().builder()
+                .title(request.getTitle())
+                .info(request.getInfo())
+                .build()
+        );
+        return this;
+    }
+
+    public SoomRoom addMember(String memberId){
+        this.memberIds.add(memberId);
+        return this;
+    }
+
+    public Notice getLatestNotice(){
+        return this.getNotices().get(getNotices().size()-1);
+    }
 
 }
