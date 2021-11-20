18ade98a98c80e415fe323a0fc3c604e3f12e4cb
jinu <jinwoo794533@gmail.com>
2021-11-20 22:58:43 +0900

    :zap: 기능 추가

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.jinwoo.snsbackend_mainserver.domain.auth.controller;
 
 
+import com.jinwoo.snsbackend_mainserver.domain.auth.exception.InvalidCodeException;
 import com.jinwoo.snsbackend_mainserver.domain.auth.payload.request.LoginRequest;
 import com.jinwoo.snsbackend_mainserver.domain.auth.payload.request.StudentSignupRequest;
+import com.jinwoo.snsbackend_mainserver.domain.auth.payload.response.MemberResponse;
 import com.jinwoo.snsbackend_mainserver.global.email.exception.EmailException;
 import com.jinwoo.snsbackend_mainserver.global.email.payload.EmailSendRequest;
 import com.jinwoo.snsbackend_mainserver.global.email.service.EmailService;
@@ -12,7 +14,9 @@ import com.jinwoo.snsbackend_mainserver.global.utils.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,10 +26,10 @@ public class AuthController {
     private final EmailService emailService;
     private final RedisUtil redisUtils;
 
+
     @GetMapping("/email")
     public String getEmailAuthentication(@RequestBody EmailSendRequest email){
         String random = String.valueOf(UUID.randomUUID()).substring(0, 5).toUpperCase();
-
         try{
             redisUtils.setDataExpire(email.getEmail(), random, 300);
             return emailService.sendEmail(email.getEmail(), random);
@@ -35,13 +39,34 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public TokenResponse signup(@RequestBody StudentSignupRequest signupRequest){
-        return authService.signup(signupRequest);
+    public TokenResponse signup(@RequestBody StudentSignupRequest signupRequest, @RequestParam String code){
+        if (redisUtils.getData(signupRequest.getId()).equals(code))
+            return authService.signup(signupRequest);
+        throw new InvalidCodeException();
     }
 
     @PostMapping("/login")
     public TokenResponse login(@RequestBody LoginRequest loginRequest){
         return authService.login(loginRequest);
     }
+
+
+    @GetMapping("/mypage")
+    public MemberResponse getMypage(){
+        return authService.mypage();
+    }
+
+
+    @GetMapping("/member/list")
+    public List<MemberResponse> getMember(){
+        return authService.getMember();
+    }
+
+
+    @GetMapping("/member")
+    public MemberResponse getSepMember(@RequestParam String memberId){
+        return authService.getSepMember(memberId);
+    }
+
 
 }
