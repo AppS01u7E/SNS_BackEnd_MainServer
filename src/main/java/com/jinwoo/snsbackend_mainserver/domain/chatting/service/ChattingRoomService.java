eb6a984adcdccd943de0ea231117c54c411e5bfa
jinu <jinwoo794533@gmail.com>
2021-11-20 23:04:17 +0900

    :zap: chatting Service

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/chatting/service/ChattingRoomService.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/chatting/service/ChattingRoomService.java
@@ -1,2 +1,16 @@
-package com.jinwoo.snsbackend_mainserver.domain.chatting.service;public interface ChattingRoomService {
+package com.jinwoo.snsbackend_mainserver.domain.chatting.service;
+
+import com.jinwoo.snsbackend_mainserver.domain.chatting.payload.request.FileMessageRequest;
+import com.jinwoo.snsbackend_mainserver.domain.chatting.payload.response.ChattingRoomListResponse;
+import com.jinwoo.snsbackend_mainserver.domain.chatting.payload.response.MessageResponse;
+
+import java.net.URL;
+import java.util.List;
+
+public interface ChattingRoomService {
+    public URL postFileMessage(FileMessageRequest request, String chattingRoomId);
+    public URL postImageMessage(FileMessageRequest request, String chattingRoomId);
+    public List<ChattingRoomListResponse> getChattingRoomList();
+    public MessageResponse getMessages(String chattingRoomId);
+
 }
