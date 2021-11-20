c29dba5ca95158821ba17b75ed330d7282514262
jinu <jinwoo794533@gmail.com>
2021-11-20 22:59:37 +0900

    :card_file_box: dao

--- src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/domain/auth/dao/MemberRepository.java
@@ -2,15 +2,20 @@ package com.jinwoo.snsbackend_mainserver.domain.auth.dao;
 
 import com.jinwoo.snsbackend_mainserver.domain.auth.entity.Member;
 import com.jinwoo.snsbackend_mainserver.domain.auth.entity.School;
+import com.jinwoo.snsbackend_mainserver.domain.soom.entity.SoomRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.config.ScheduledTaskHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+
 public interface MemberRepository extends JpaRepository<Member, String> {
     //callingService
     List<Member> findAllBySchool(School school);
     Optional<Member> findBySchoolAndId(School school, String id);
+    List<Member> findAllBySchoolAndGradeAndClassNum(School school, int grade, int classNum);
+    List<Member> findAllBySchoolAndSoomRoomsContaining(School school, SoomRoom club);
+    List<Member> findAllBySchoolAndGrade(School school, int grade);
 }
