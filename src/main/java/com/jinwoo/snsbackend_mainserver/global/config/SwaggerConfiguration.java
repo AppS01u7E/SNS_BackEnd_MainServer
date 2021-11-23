3b5e7df45b5ab0716093f9646904be4ff6800233
jinu <jinwoo794533@gmail.com>
2021-11-24 00:52:46 +0900

    :sparkles: swagger

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/config/SwaggerConfiguration.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/config/SwaggerConfiguration.java
@@ -1,6 +1,41 @@
 package com.jinwoo.snsbackend_mainserver.global.config;
 
+import io.swagger.models.parameters.Parameter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
 public class SwaggerConfiguration {
 
+    @Bean
+    public Docket api() {
 
+
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SNS_RESTAPI")
+                .description("SchoolNetworkService")
+                .version("BETA")
+                .build();
+    }
 }
