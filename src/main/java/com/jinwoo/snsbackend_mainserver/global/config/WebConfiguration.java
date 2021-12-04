e9080c8c0bbfd79b36e938cc99fd40b50156c70d
jinu <jinwoo794533@gmail.com>
2021-12-04 21:41:21 +0900

    :zap: CORS 해결

--- src/main/java/com/jinwoo/snsbackend_mainserver/global/config/WebConfiguration.java
+++ src/main/java/com/jinwoo/snsbackend_mainserver/global/config/WebConfiguration.java
@@ -1,2 +1,24 @@
-package com.jinwoo.snsbackend_mainserver.global.config;public class WebConfiguration {
+package com.jinwoo.snsbackend_mainserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry
+                .addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","POST","PATCH","DELETE","PUT","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+
+    }
+
 }
